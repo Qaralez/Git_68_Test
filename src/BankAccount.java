5a97c732af860f2e31dbe4f784b1102906328991
Qaralez <ayder.dudakov@icloud.com>
2025-06-22 15:27:01 +0200

    Добавлены Java-классы и конфигурационные файлы

---
+++ src/BankAccount.java
@@ -0,0 +1,16 @@
+
+
+public class BankAccount {
+    String iban;    // номер счета
+    double balance; // баланс счета
+    Person owner;   // владелец счета
+    MyDate open;    //дата открытия
+
+    public BankAccount(String ibanStr, double b, Person ownerPerson, MyDate opDate) {
+        iban = ibanStr;
+        balance = b;
+        owner = ownerPerson;
+        open = opDate;
+    }
+
+}
